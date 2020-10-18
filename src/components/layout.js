ccf917ea3c86d81306d3ae8c57d4c9c623ce9617
uttam-hear-kr <uttam.thapa@hear.com>
2020-10-19 01:53:44 +0530

    changes done

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,14 +26,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-        <footer className="overflow-hidden">
-					<div className="container mx-auto px-8 md:px-16 pt-16 pb-10 relative z-10 ">
-          	<p className="text-white">&copy; {new Date().getFullYear()}, {` `} Stitch &amp; Style  | Design &amp; Developed by <a href="https://www.ukthapa.com" target="_blank">ukthapa.com</a></p>
-					</div>
-        </footer>
-      </div>
+			<main>{children}</main>
+			<footer className="overflow-hidden">
+				<div className="container mx-auto px-8 md:px-16 pt-16 pb-10 relative z-10 ">
+					<p className="text-white">&copy; {new Date().getFullYear()}, {` `} Stitch &amp; Style  | Design &amp; Developed by <a href="https://www.ukthapa.com" target="_blank">ukthapa.com</a></p>
+				</div>
+			</footer>
     </>
   )
 }
