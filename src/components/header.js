c8ecb21d1d6f2e84d80117309d0c63f12da352a7
uttam-hear-kr <uttam.thapa@hear.com>
2021-01-02 11:42:57 +0530

    nav mobile issue fixed

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import PrimaryNAV from "./primarynav.js"
 
 const Header = ({ siteTitle }) => (
   <header className="fixed w-full transition-shadow duration-500 ease-in-out z-50">
-    <div className="container mx-auto px-2  md:px-16 md:py-3">
+    <div className="container mx-auto px-2 py-2 md:px-16 md:py-3">
 			<PrimaryNAV />
     </div>
   </header>
